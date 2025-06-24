2e2026a457b06c32c273f859eaf9d24782f678a1
seunghyun <ppdoli123@naver.com>
2025-06-24 14:32:20 +0900

    로컬서버연결완료

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:8080", // 백엔드 주소에 맞게 필요시 수정
+  baseURL: "http://finland.r-e.kr:8080", // 백엔드 주소에 맞게 필요시 수정
   headers: {
     Authorization:
-      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJmZThiMzZjZC1iMmUxLTRhYTgtOGVkZS1iMDdhMjM1OWFhZDkiLCJpYXQiOjE3NTA3MzA3OTgsImV4cCI6MTc1MDgxNzE5OH0.TW98PHXyW_jLFDlgqR1MkWb074kKDPJAN3gOgCWsbKQ",
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIwZmMyMDRkZi1hMzkxLTQ5OTItOTZmYS04YzdkYTZmODRkZTUiLCJpYXQiOjE3NTA3NDExNTUsImV4cCI6MTc1MDgyNzU1NX0.eW3LU3BIAcH1fcMFdK0N9iTAprxmIP1pL_eAj5yZjbg",
     "Content-Type": "application/json",
   },
   withCredentials: true,
